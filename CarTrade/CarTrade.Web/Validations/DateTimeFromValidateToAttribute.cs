a096e2fe9cf07d5246433d5bb476c461b8b43238
mindball <mincho.balaliew@gmail.com>
2021-05-13 09:28:52 +0300

    Custom Date validation: IValidatableObject then Custom built-in attributes (ValidationAttribute)

---
+++ CarTrade/CarTrade.Web/Validations/DateTimeFromValidateToAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTrade.Web.Validations
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DateTimeFromValidateToAttribute : ValidationAttribute
+    {
+        public DateTimeFromValidateToAttribute(string dateToCompareToFieldName)
+        {
+            this.DateToCompareToFieldName = dateToCompareToFieldName;
+        }
+
+        private string DateToCompareToFieldName { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime earlierDate = (DateTime)value;
+
+            DateTime laterDate = (DateTime)validationContext.ObjectType.GetProperty(this.DateToCompareToFieldName).GetValue(validationContext.ObjectInstance, null);
+
+            if (laterDate > earlierDate)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Later date  is equal or less than Start date");
+
+        }
+    }
+}
