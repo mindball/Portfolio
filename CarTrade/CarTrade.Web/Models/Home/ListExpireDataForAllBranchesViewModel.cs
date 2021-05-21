a3f7d1e0e8ce128f6f5fd6f45f2be77b6c70291d
mindball <mincho.balaliew@gmail.com>
2021-05-21 12:23:23 +0300

    add models

--- CarTrade/CarTrade.Web/Models/Home/ListExpireDataForAllBranchesViewModel.cs
+++ CarTrade/CarTrade.Web/Models/Home/ListExpireDataForAllBranchesViewModel.cs
@@ -1,16 +1,19 @@
 ﻿using CarTrade.Services.Vehicle.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace CarTrade.Web.Models.Home
 {
     public class ListExpireDataForAllBranchesViewModel
     {
+        public int BranchId { get; set; }
+
         public string FullAddress { get; set; }
+        
+        public IList<VehicleListExpireInsurancePolicyServiceModel> VehiclesWithExpirePolicy { get; set; }
 
-        public IEnumerable<VehicleListExpireInsurancePolicyServiceModel> VehiclesWithExpirePolicy { get; set; }
+        public IList<VehicleListExpireVignetteServiceModel> VehiclesWithExpireVignettes { get; set; }
 
-        public IEnumerable<VehicleListExpireVignetteServiceModel> VehiclesWithExpireVignettes { get; set; }
-
-        public IEnumerable<VehicleListingChangeOilServiceModel> VehiclesWithOilChangeDistance { get; set; }
+        public IList<VehicleListingChangeOilServiceModel> VehiclesWithOilChangeDistance { get; set; }
     }
 }
